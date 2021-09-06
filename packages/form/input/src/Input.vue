64988ea2e1f11d55e9d7d8f7b16c0526f5f8d9fe
caojun <619635878@qq.com>
2021-09-06 11:19:37 +0800

    [style]保存

--- packages/form/input/src/Input.vue
+++ packages/form/input/src/Input.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <input type="text">
+  <div class="base-input">
+    <input
+      type="text"
+      class="base-input__inner"
+    >
   </div>
 </template>
 <script>
@@ -12,3 +15,28 @@ export default defineComponent({
   }
 })
 </script>
+<style lang="less" scoped>
+.base-input{
+  position: relative;
+  font-size: 14px;
+  display: inline-block;
+  width: 100%;
+  .base-input__inner{
+    -webkit-appearance: none;
+    background-color: #fff;
+    background-image: none;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 40px;
+    line-height: 40px;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    width: 100%;
+  }
+}
+</style>
